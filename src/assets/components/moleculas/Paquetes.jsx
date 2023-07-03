5faad1ae6454d590196a3b5b0a02dfdf9c1306a6
DarioAntonio <antoniodario10@hotmail.com>
2023-07-03 12:20:03 -0600

    Pauqtes

--- src/assets/components/moleculas/Paquetes.jsx
+++ src/assets/components/moleculas/Paquetes.jsx
@@ -5,61 +5,65 @@ function Paquetes() {
     return ( 
         <>
         
-<section id="pricing" class="pricing">
-<div class="container aos-init aos-animate" data-aos="fade-up">
-  <div class="row gy-4 aos-init aos-animate" data-aos="fade-left">
-    <div class="col-lg-3 col-md-6 aos-init text-center justify-content-center aos-animate" data-aos="zoom-in" data-aos-delay="100">
-      <div class="box">
-        <h3>Paquete 1</h3>
-        <div class="price"><sup>$</sup>4,000</div>
-        <img src={logoCard} class="imagenLogo img-fluid" alt=""/>
-        <center><ul className="textPaquete">
-            <lo class="textPaquete">Limite máximo de personas por evento 120.</lo>
-            <lo>Limite mínimo de personas por evento 60.</lo>
-            <lo>El precio aplica solo de lunes a miercoles.</lo>
-            <lo>Preguntar sobre los servicios.</lo>
-        </ul></center>
+<section id="pricing" className="pricing">
+<div className="container aos-init aos-animate" data-aos="fade-up">
+  <div className="row gy-4 aos-init aos-animate" data-aos="fade-left">
+    <div className="col-lg-3 col-md-6 aos-init text-center justify-content-center aos-animate" data-aos="zoom-in" data-aos-delay="100">
+      <div className="box">
+        <br/><h3 className="tituloPaquete">Paquete 1</h3>
+        <div className="price"><sup>$</sup>4,000</div>
+        <img src={logoCard} className="imagenLogo img-fluid" alt=""/>
+        <ul className="textPaquete">
+            <lo>Limite máximo de personas por evento 120.</lo><br/>
+            <lo>Limite mínimo de personas por evento 60.</lo><br/>
+            <lo>El precio aplica solo de <b>lunes</b> a <b>miercoles</b>.</lo><br/>
+            <lo>Preguntar sobre los servicios.</lo><br/>
+        </ul>
+        <button className="btn-buy">Reservar</button>
     </div>
 </div>
 
-    <div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="200">
-      <div class="box">
-        <h3>Paquete 2</h3>
-        <div class="price"><sup>$</sup>5,500</div>
-        <img src="assets/img/pricing-starter.png" class="img-fluid" alt=""/>
-        <ul>
-            <lo>Limite máximo de personas por evento 120.</lo>
-            <lo>Limite mínimo de personas por evento 60.</lo>
-            <lo>El precio aplica solo de viernes a sabado.</lo>
-            <lo>Preguntar sobre los servicios.</lo>
+    <div className="col-lg-3 col-md-6 aos-init aos-animate text-center justify-content-center" data-aos="zoom-in" data-aos-delay="200">
+      <div className="box">
+      <br/><h3 className="tituloPaqueteTop">Paquete 2</h3>
+        <div className="price"><sup>$</sup>5,500</div>
+        <img src={logoCard} className="imagenLogo img-fluid" alt=""/>
+        <ul className="textPaquete">
+            <lo>Limite máximo de personas por evento 120.</lo><br/>
+            <lo>Limite mínimo de personas por evento 60.</lo><br/>
+            <lo>El precio aplica solo de <b>viernes</b> a <b>sabado</b>.</lo><br/>
+            <lo>Preguntar sobre los servicios.</lo><br/>
         </ul>
+        <button className="btn-buyTop">Reservar</button>
       </div>
     </div>
 
-    <div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="300">
-      <div class="box">
-        <h3 >Paquete 3</h3>
-        <div class="price"><sup>$</sup>4,500</div>
-        <img src="assets/img/pricing-business.png" class="img-fluid" alt=""/>
-        <ul>
-            <lo>Limite máximo de personas por evento 120.</lo>
-            <lo>Limite mínimo de personas por evento 60.</lo>
-            <lo>El precio aplica solo de jueves a domingo.</lo>
-            <lo>El precio aplica solo de jueves a domingo.</lo>
+    <div className="col-lg-3 col-md-6 aos-init aos-animate text-center justify-content-center" data-aos="zoom-in" data-aos-delay="300">
+      <div className="box">
+      <br/><h3 className="tituloPaquete">Paquete 3</h3>
+        <div className="price"><sup>$</sup>4,500</div>
+        <img src={logoCard} className="imagenLogo img-fluid" alt=""/>
+        <ul className="textPaquete">
+            <lo>Limite máximo de personas por evento 120.</lo><br/>
+            <lo>Limite mínimo de personas por evento 60.</lo><br/>
+            <lo>El precio aplica solo de <b>jueves</b> a <b>domingo</b>.</lo><br/>
+            <lo>Preguntar sobre los servicios.</lo><br/>
         </ul>
+        <button className="btn-buy">Reservar</button>
       </div>
     </div>
 
-    <div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="400">
-      <div class="box">
-        <h3>Paquete por persona</h3>
-        <div class="price"><sup>$</sup>350</div>
-        <img src="assets/img/pricing-ultimate.png" class="img-fluid" alt=""/>
-        <ul>
-            <lo>Limite máximo de personas por evento 120.</lo>
-            <lo>Limite mínimo de personas por evento 60.</lo>
-            <lo>Cuenta con todos los servicios.</lo>
+    <div className="col-lg-3 col-md-6 aos-init aos-animate text-center justify-content-center" data-aos="zoom-in" data-aos-delay="400">
+      <div className="box">
+      <br/><h3 className="tituloPaquete">Paquete por persona</h3>
+        <div className="price"><sup>$</sup>350</div>
+        <img src={logoCard} className="imagenLogo img-fluid" alt=""/>
+        <ul className="textPaquete">
+            <lo>Limite máximo de personas por evento 120.</lo><br/>
+            <lo>Limite mínimo de personas por evento 60.</lo><br/>
+            <lo>Cuenta con todos los servicios.</lo><br/><br/>
         </ul>
+        <button className="btn-buy">Reservar</button>
       </div>
     </div>
   </div>
