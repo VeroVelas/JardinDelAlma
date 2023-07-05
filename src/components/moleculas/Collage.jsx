febe3e404dad0eeafaf3d2b0aa37eb6e93b3295e
VeroVelas <veronicavelasco2778@gmail.com>
2023-07-05 15:55:55 -0600

    collage

--- src/components/moleculas/Collage.jsx
+++ src/components/moleculas/Collage.jsx
@@ -5,48 +5,38 @@ import img4c from "../../assets/img/img4c.png"
 import img5c from "../../assets/img/img5c.png"
 import img6c from "../../assets/img/img6c.png"
 import img7c from "../../assets/img/img7c.png"
+import imagenSection1 from "../../assets/img/imagenSection1.png"
 import "../../assets/styles/Collage.css"
 
 function Collage() {
   return (
     <>
-    {/*<div className="row">
-      <div className="column">
-        <img src={img1c} alt="" />
-        <img src={img2c} alt="" />
-        <img src={img3c} alt="" />
-        <img src={img4c} alt="" />
-        <img src={img5c} alt="" />
-        <img src={img6c} alt="" />
-        <img src={img7c} alt="" />
-      </div>
-    </div>
-      <div className="collage">
-        <div className="imagen1">
-          <img src={img1c} alt="Imagen 1" />
-        </div>
-
-        <div className="imagen2">
-          <img src={img2c} alt="Imagen 2" />
-        </div>
-        <div className="imagen3">
-          <img src={img3c} alt="Imagen 3" />
-        </div>
-        
-        <div className="imagen4">
-          <img src={img4c} alt="Imagen 4" />
-        </div>
-
-        <div className="imagen5">
-          <img src={img5c} alt="Imagen 5" />
-        </div>
-         <div className="imagen6">
-          <img src={img6c} alt="Imagen 6" />
-        </div>
-        <div className="imagen7">
-          <img src={img7c} alt="Imagen 7" />
-        </div>
-      </div>*/}
+      <section class="gallery">
+        <figure class="gallery__picture gallery__picture--g1">
+          <img src={img1c} alt="" class="gallery__img" />
+        </figure>
+        <figure class="gallery__picture gallery__picture--g2">
+          <img src={img2c} alt="" class="gallery__img" />
+        </figure>
+        <figure class="gallery__picture gallery__picture--g3">
+          <img src={img3c} alt="" class="gallery__img" />
+        </figure>
+        <figure class="gallery__picture gallery__picture--g4">
+          <img src={img5c} alt="" class="gallery__img" />
+        </figure>
+        <figure class="gallery__picture gallery__picture--g5">
+          <img src={img7c} alt="" class="gallery__img" />
+        </figure>
+        <figure class="gallery__picture gallery__picture--g6">
+          <img src={img6c} alt="" class="gallery__img" />
+        </figure>
+        <figure class="gallery__picture gallery__picture--g7">
+          <img src={img4c} alt="" class="gallery__img" />
+        </figure>
+        <figure class="gallery__picture gallery__picture--g8">
+          <img src={imagenSection1} alt="" class="gallery__img" />
+        </figure>
+      </section>
     </>
   );
 }
