8f50d2836f2db21e601894184159dfc6a40d8bd8
DarioAntonio <antoniodario10@hotmail.com>
2023-07-06 09:47:15 -0600

    [FIX] el collage y las imagenes

--- src/components/moleculas/Collage.jsx
+++ src/components/moleculas/Collage.jsx
@@ -1,42 +1,58 @@
 import img1c from "../../assets/img/img1c.png"
 import img2c from "../../assets/img/img2c.png"
 import img3c from "../../assets/img/img3c.png"
-import img4c from "../../assets/img/img4c.png"
 import img5c from "../../assets/img/img5c.png"
 import img6c from "../../assets/img/img6c.png"
 import img7c from "../../assets/img/img7c.png"
-import imagenSection1 from "../../assets/img/imagenSection1.png"
+import imgCollageGrande from "../../assets/img/imagenGrandeCollage.png";
 import "../../assets/styles/Collage.css"
 
 function Collage() {
   return (
     <>
-      <section class="gallery">
-        <figure class="gallery__picture gallery__picture--g1">
-          <img src={img1c} alt="" class="gallery__img" />
-        </figure>
-        <figure class="gallery__picture gallery__picture--g2">
-          <img src={img2c} alt="" class="gallery__img" />
-        </figure>
-        <figure class="gallery__picture gallery__picture--g3">
-          <img src={img3c} alt="" class="gallery__img" />
-        </figure>
-        <figure class="gallery__picture gallery__picture--g4">
-          <img src={img5c} alt="" class="gallery__img" />
-        </figure>
-        <figure class="gallery__picture gallery__picture--g5">
-          <img src={img7c} alt="" class="gallery__img" />
-        </figure>
-        <figure class="gallery__picture gallery__picture--g6">
-          <img src={img6c} alt="" class="gallery__img" />
-        </figure>
-        <figure class="gallery__picture gallery__picture--g7">
-          <img src={img4c} alt="" class="gallery__img" />
-        </figure>
-        <figure class="gallery__picture gallery__picture--g8">
-          <img src={imagenSection1} alt="" class="gallery__img" />
-        </figure>
-      </section>
+    <img src={imgCollageGrande} loading="lazy" className="coverImagen"/>
+    <div class="lightbox">
+  <div class="row">
+    <div class="col-lg-6">
+      <img
+        src={img1c}
+        loading="lazy"
+        class="w-100 mb-2 mb-md-4 shadow-1-strong rounded"
+      />
+      <img
+        src={img6c}
+        loading="lazy"
+        class="w-100 shadow-1-strong rounded"
+      />
+    </div>
+    <div class="col-lg-6">
+      <img
+        src={img3c}
+        loading="lazy"
+        class="w-100 mb-2 mb-md-4 shadow-1-strong rounded"
+      />
+      <img
+        src={img2c}
+        loading="lazy"
+        class="w-100 shadow-1-strong rounded"
+      />
+    </div>
+    <div class="col-lg-6 p-3">
+      <img
+        src={img7c}
+        loading="lazy"
+        class="w-100 shadow-1-strong rounded"
+      />
+    </div>
+    <div class="col-lg-6 p-3">
+    <img
+        src={img5c}
+        loading="lazy"
+        class="w-100 mb-2 mb-md-4 shadow-1-strong rounded"
+      />
+    </div>
+  </div>
+</div>
     </>
   );
 }
