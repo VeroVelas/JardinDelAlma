9198b178567ce26ff553b9728626dc8e9b2f52e0
DarioAntonio <antoniodario10@hotmail.com>
2023-07-08 11:41:59 -0600

    [ADD] tres cajas de texto formulario

--- src/pages/Formulario.jsx
+++ src/pages/Formulario.jsx
@@ -6,12 +6,12 @@ function Formulario() {
         <>
         <Navbar/>
         <div className="FondoVerde">
-        <center><div className="FondoBlancoForm">
-                    <div className="tamanoCardForm">
-                        <br/><h3 className="TituloFormulario">Formulario</h3>
-                        <p className="texto1Formualrio">Ingrese sus datos personales de tal manera que el dueño del salón podrá comunicarse con usted y personalizar su evento.</p>
+    <center><div className="FondoBlancoForm">
+                <div className="tamanoCardForm">
+                <br/><h3 className="TituloFormulario">Formulario</h3>
+                    <p className="texto1Formualrio">Ingrese sus datos personales de tal manera que el dueño del salón podrá comunicarse con usted y personalizar su evento.</p>
                         <div className="">
-                                <form>
+                            <form>
                                 <center>
                                     <div className="">
                                         <input type="email" placeholder="Nombre Completo" className="inputFormulario "/>
@@ -33,11 +33,34 @@ function Formulario() {
                                     </div>
                                     <center><button type="submit" className="btn">Enviar</button></center>		
                                 </center>
-                                </form>
-                            </div>
+                            </form>
                         </div>
-                    </div></center>
+                    </div>
+                </div></center>
+            </div>
+            <div class="card-group">
+                <div class="card">
+                    <div class="CajaInformacion card-body">
+                        <h5 class="tituloNegritas card-title text-center">Nota</h5>
+                        <p class="textoJustificado card-text">
+                            El paquete por persona mínimo deben albergar 60 personas.
+                            Al igual que el precio será un estimado en invitados, para ello ponerse en contacto en el dueño.
+                        </p>
+                    </div>
                 </div>
+                <div class="card">
+                    <div class="CajaInformacion card-body">
+                        <h5 class="tituloNegritas card-title text-center">Precio estimado de invitados</h5>
+                        <h3>$</h3>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="CajaInformacion card-body">
+                        <h5 class="tituloNegritas card-title text-center">Nota</h5>
+                        <p class="textoJustificado card-text text-justify">En el paquete 1,2 y 3 cada invitado tiene un costo de $315, ya que se verá reflejado el precio estimado en cuantos invitados aproximados designe en su paquete + el precio base del salón (Paquete de su elección).</p>
+                    </div>
+                </div>
+            </div>
         </>
      );
 }
