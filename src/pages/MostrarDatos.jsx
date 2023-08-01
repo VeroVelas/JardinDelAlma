6736bb4f32567281138ffab52be34d5f9f3e4977
DarioAntonio <antoniodario10@hotmail.com>
2023-07-31 23:44:55 -0600

    [ADD] alerts estilizadas y pdf

--- src/pages/MostrarDatos.jsx
+++ src/pages/MostrarDatos.jsx
@@ -4,7 +4,9 @@ import Clientes from '../components/contenedor/Clientes';
 import Data from "../components/atoms/Data";
 import Navbar from "../components/moleculas/Navbar";
 import Title2 from "../components/atoms/Title2";
+import Swal from 'sweetalert2'
 import '../assets/styles/MostrarDatos.css';
+
 
 function MostrarDatos() { 
     const componentPDF = useRef();
@@ -17,7 +19,13 @@ function MostrarDatos() {
     const generatePDF = useReactToPrint({
         content: () => componentPDF.current,
         documentTitle:"Userdata",
-        onAfterPrint:()=>alert("Datos guardados en el PDF")
+        onAfterPrint:() => Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'PDF Guardado',
+            showConfirmButton: false,
+            timer: 1500
+          })
     });
     return (
         <>
@@ -26,37 +34,37 @@ function MostrarDatos() {
             <br/><br/><br/><br/>
             <div>
                 <div ref={componentPDF} style={{width:'100%'}}>
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th><h3>Nombre</h3></th>
-                            <th><h3>Teléfono</h3></th>
-                            <th><h3>Invitados</h3></th>
-                            <th><h3>Fecha</h3></th>
-                            <th><h3>Evento</h3></th>
-                            <th><h3>Paquete</h3></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            datos.map((cliente)=>(
-                                <>
-                                    <Data
-                                        nombre={cliente.nombre}
-                                        invitados={cliente.invitados}
-                                        telefono={cliente.telefono}
-                                        fecha={cliente.fecha}
-                                        evento={cliente.evento}
-                                        paquete={cliente.paquete}
-                                    />
-                                </>
-                            ))
-                        }
-                    </tbody>
-                    <div>
-                        <button className=" btn btn-success" onClick={generatePDF}>PDF</button>
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th><h3>Nombre</h3></th>
+                                <th><h3>Teléfono</h3></th>
+                                <th><h3>Invitados</h3></th>
+                                <th><h3>Fecha</h3></th>
+                                <th><h3>Evento</h3></th>
+                                <th><h3>Paquete</h3></th>
+                            </tr><br/>
+                        </thead>
+                        <tbody>
+                            {
+                                datos.map((cliente)=>(
+                                    <>
+                                        <Data
+                                            nombre={cliente.nombre}
+                                            invitados={cliente.invitados}
+                                            telefono={cliente.telefono}
+                                            fecha={cliente.fecha}
+                                            evento={cliente.evento}
+                                            paquete={cliente.paquete}
+                                        />
+                                    </>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                    <div className="posisionBtn">
+                    <button  onClick={generatePDF}>PDF</button>
                     </div>
-                </table>
                 </div>
             </div>
         </>
